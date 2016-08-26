190b2c226a4a3e0b9519d0f1933cf976f8088a89
Tim Meinhardt <meinhardt.tim@gmai.com>
2016-08-26 13:52:54 +0200

    Implement basic patch_BM3D prox_fns

--- proximal/prox_fns/__init__.py
+++ proximal/prox_fns/__init__.py
@@ -5,6 +5,7 @@ from .norm1 import norm1, weighted_norm1
 from .sum_squares import sum_squares, weighted_sum_squares, least_squares, lsqr_options, cg_options
 from .group_norm1 import group_norm1, weighted_group_norm1
 from .poisson_norm import poisson_norm, weighted_poisson_norm
+from .patch_BM3D import patch_BM3D
 from .patch_NLM import patch_NLM
 from .sum_entries import sum_entries, zero_prox
 # from .matlab_external import matlab_external
---
+++ proximal/prox_fns/patch_BM3D.py
@@ -0,0 +1,52 @@
+from .prox_fn import ProxFn
+import numpy as np
+import pybm3d
+
+
+class patch_BM3D(ProxFn):
+    """The function for BM3D patch prior
+    """
+
+    def __init__(self, lin_op, sigma=0.04,
+                 templateWindowSizeBM3D=3, searchWindowSizeBM3D=11, **kwargs):
+
+        # Check for the shape
+        if not (len(lin_op.shape) == 2 or len(lin_op.shape) == 3 and lin_op.shape[2] in [1, 3]):
+            raise ValueError('BM3D needs a 3 or 1 channel image')
+
+        self.sigma = sigma
+        self.templateWindowSizeBM3D = templateWindowSizeBM3D
+        self.searchWindowSizeBM3D = searchWindowSizeBM3D
+
+        super(patch_BM3D, self).__init__(lin_op, **kwargs)
+
+    def _prox(self, rho, v, *args, **kwargs):
+        """x = denoise_gaussian_BM3D( tonemap(v), sqrt(1/rho))
+        """
+
+        # Scale d
+        v = v.copy()
+
+        if len(v.shape) == 2:
+            v = v.reshape(v.shape  + (1,))
+
+        dst = np.array(pybm3d.bm3d.bm3d(v.astype(np.float32), self.sigma))
+        dst = np.nan_to_num(dst).astype(v.dtype)
+
+        np.copyto(v, dst)
+
+        return np.squeeze(v)
+
+    def _eval(self, v):
+        """Evaluate the function on v (ignoring parameters).
+        """
+        return np.inf  # TODO: IGNORE FOR NOW
+
+    def get_data(self):
+        """Returns info needed to reconstruct the object besides the args.
+
+        Returns
+        -------
+        list
+        """
+        return [self.sigma, self.templateWindowSizeBM3D, self.searchWindowSizeBM3D]
