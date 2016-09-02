7b183833262b55baf96880a5adbb9a2f5607a162
Tim Meinhardt <meinhardt.tim@gmai.com>
2016-09-02 13:19:20 +0200

    Implement prox black box draft

---
+++ proximal/prox_fns/prox_black_box.py
@@ -0,0 +1,29 @@
+from .prox_fn import ProxFn
+import numpy as np
+
+class prox_black_box(ProxFn):
+    """A black-box proxable function specified by the user.
+    """
+
+    def __init__(self, lin_op, prox, **kwargs):
+        self.__prox = prox
+        super(prox_black_box, self).__init__(lin_op, **kwargs)
+
+    def _prox(self, rho, v, *args, **kwargs):
+        """The prox operator.
+        """
+        return self.__prox(v)
+
+    def _eval(self, v):
+        """Evaluate the function on v (ignoring parameters).
+        """
+        return np.inf  # TODO: IGNORE FOR NOW
+
+    def get_data(self):
+        """Returns info needed to reconstruct the object besides the args.
+
+        Returns
+        -------
+        list
+        """
+        return []   # TODO: IGNORE FOR NOW
